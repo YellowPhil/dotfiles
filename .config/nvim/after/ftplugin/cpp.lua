d3b49b95aed0da8ad0429cb4f50e301a2367441d
synerr <synerr@syner.ru>
2023-12-07 12:28:07 +0300

    config update

--- .config/nvim/after/ftplugin/cpp.lua
+++
@@ -1,5 +0,0 @@
-local keymap = vim.keymap.set
-
-keymap("n", "<leader>r", [[:TermExec cmd="clear && g++ -g -fsanitize=address -std=c++17 -Wall -Wextra -Wshadow -DONPC -O2 %:p -o %:p:r && %:p:r"<cr>]], {silent = true, buffer = true})
-
-keymap("n", "<leader>b", [[:TermExec cmd="make"<cr>]], {silent=true, buffer=true})
