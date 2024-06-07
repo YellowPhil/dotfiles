4a9a7438c2c4a89a93f1ea23e66937800752d548
Synerr <9093704339@list.ru>
2024-06-07 20:19:42 +0300

    config update

--- .config/fish/custom_imports/path.fish
+++ .config/fish/custom_imports/path.fish
@@ -1,6 +1,5 @@
 fish_add_path -g $HOME/.local/bin/
 fish_add_path -g $HOME/go/bin/
+fish_add_path -g $HOME/.cargo/bin/
 fish_add_path -g $NPM_PACKAGES/bin/
 fish_add_path -g $HOME/yandex-cloud/bin/
-
-
